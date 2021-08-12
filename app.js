db174dcf5ae183b4b10c5bfff626c6eb425a9278
Ankit Sahu <sahu6856@gmail.com>
2021-08-12 23:35:03 +0530

    fix routing problem

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(require('./routers/auth'));
 
 if(process.env.NODE_ENV==="production"){
     const path = require('path');
-    app.get('/',(req,res)=>{
-        app.use(express.static(path.resolve(__dirname,'client','build')));
+    app.use(express.static(path.resolve(__dirname,'client','build')));
+    app.get('/*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
 }
